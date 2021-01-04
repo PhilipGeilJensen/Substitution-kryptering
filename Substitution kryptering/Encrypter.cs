e882112fd122bfa2309288ba9a42ad8ea3aeaf5b
PhilipGeil <pgj.philip@gmail.com>
2021-01-04 11:43:35 +0100

    Initial commit

---
+++ Substitution kryptering/Encrypter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Substitution_kryptering
+{
+    class Encrypter
+    {
+        static List<string> alphabet = new List<string>() { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "æ", "ø", "å" };
+
+        /// <summary>
+        /// Method to encrypt the provided string
+        /// </summary>
+        /// <param name="text"> text to encrypt</param>
+        /// <returns>Encrypted text</returns>
+        public static string Encrypt(string text)
+        {
+            StringBuilder s = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                bool isUpper = Char.IsUpper(text[i]);
+                char c = Char.ToLower(text[i]);
+                if (alphabet.Contains(c.ToString()))
+                {
+                    int index;
+                    int current = alphabet.IndexOf(c.ToString());
+                    if (current + 10 >= alphabet.Count)
+                    {
+                        index = (current + 10) - alphabet.Count;
+                    }
+                    else
+                    {
+                        index = current + 10;
+                    }
+                    if (isUpper)
+                    {
+                        c = Char.ToUpper(char.Parse(alphabet[index]));
+                    } else
+                    {
+                        c = char.Parse(alphabet[index]);
+                    }
+                    s.Append(c);
+                } else
+                {
+                    s.Append(text[i].ToString());
+                }
+            }
+            return s.ToString();
+        }
+
+        /// <summary>
+        /// Method to decrypt an encrypted text
+        /// </summary>
+        /// <param name="text">The text to decrypt</param>
+        /// <returns>The decrypted text</returns>
+        public static string Decrypt(string text)
+        {
+            StringBuilder s = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                bool isUpper = Char.IsUpper(text[i]);
+                char c = Char.ToLower(text[i]);
+                if (alphabet.Contains(c.ToString()))
+                {
+                    int index;
+                    int current = alphabet.IndexOf(c.ToString());
+                    if (current - 10 <= 0)
+                    {
+                        index = (current - 10) + alphabet.Count;
+                    }
+                    else
+                    {
+                        index = current - 10;
+                    }
+                    if (isUpper)
+                    {
+                        c = Char.ToUpper(char.Parse(alphabet[index]));
+                    }
+                    else
+                    {
+                        c = char.Parse(alphabet[index]);
+                    }
+                    s.Append(c);
+                }
+                else
+                {
+                    s.Append(text[i].ToString());
+                }
+            }
+            return s.ToString();
+        }
+    }
+}
---
+++ Substitution kryptering/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Substitution_kryptering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string enc = Encrypter.Encrypt("Vi kom, Vi så, Vi sejrede");
+            string dec = Encrypter.Decrypt(enc);
+            Console.WriteLine("Enkrypteret: {0}", enc);
+            Console.WriteLine("Dekrypteret: {0}", dec);
+        }
+    }
+}
